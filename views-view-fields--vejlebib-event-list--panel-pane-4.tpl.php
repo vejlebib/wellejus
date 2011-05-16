d754f702117f44f13207ced9fe54a3354b91aef2
Ewan Andreasen <ea@vejlebib.dk>
2011-05-16 15:15:52 +0200

    Vejle Bibliotekernes tema til ding baseret på Dynamo

---
+++ views-view-fields--vejlebib-event-list--panel-pane-4.tpl.php
@@ -0,0 +1,27 @@
+<?php
+// $Id$
+
+// Prepare a couple of variables.
+$start = date_make_date($fields['field_datetime_value']->raw);
+$price = ($fields['field_entry_price_value']->raw < 1) ? t('Free') : intval($fields['field_entry_price_value']->raw) . ' kr.';
+?>
+<div class="calendar-leafs">
+
+  <div class="leaf <?php print $is_today ?> <?php print $fields['field_library_ref_nid_1']->content; ?>">
+    <div class="day"><?php print dynamo_datef($start, 'l');?></div>
+    <div class="date"><?php print dynamo_datef($start, 'j');?></div>
+    <div class="month"><?php print dynamo_datef($start, 'M');?></div>
+  </div>
+  
+  <div class="info">
+    <span><?php print $fields['field_library_ref_nid']->content; ?></span>
+    <h4><?php print $fields['title']->content; ?></h4>
+    <?php if (dynamo_datef($start, 'Hi') != '0000'): ?>
+    <span class="time">
+			<?php print dynamo_datef($start, 'H:i'); ?> -
+    </span>
+    <?php endif; ?>
+    <?php print $price; ?>
+  </div>
+
+</div>
