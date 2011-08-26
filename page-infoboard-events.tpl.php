c311b9e65e8f56ffeb1f4a9ee3a442f8b64b10c6
Ewan Andreasen <ea@vejlebib.dk>
2011-08-26 16:49:05 +0200

    modified template for infoboard and removed view templates for code simplicity

--- page-infoboard-events.tpl.php
+++ page-infoboard-events.tpl.php
@@ -2,6 +2,29 @@
 // $Id$
 ?>
 <html>
+<head>
+  <style>
+  body {
+    font: 26px verdana, sans-serif;
+    padding-left:20px;
+    color:white;
+    }
+  
+  body div.view {
+	width:375px;
+  }  
+  
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    } 
+  li { 
+    margin-bottom: 1em;
+    }
+    
+  </style>
+</head>
 <body>
 
     <?php print $content; ?>
