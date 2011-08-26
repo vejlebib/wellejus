ec09edc3f3f6d8fb2fcc3ce3220f8b5a05e61e34
Ewan Andreasen <ea@vejlebib.dk>
2011-08-26 16:49:59 +0200

    removed view templates for infoboard - not needed anymore

--- views-view--vejlebib-event-list--page.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id: views-view.tpl.php,v 1.13.2.2 2010/03/25 20:25:28 merlinofchaos Exp $
-?>
-  
-  <?php if ($rows): ?>
-    <div class="view-content">
-      <?php print $rows; ?>
-    </div>
-  <?php endif; ?>
