eea8eacf276f68a00d0463aa6b6cf7973e0200c4
Simon Holt <simoh@vejlebib.com>
2014-04-24 09:11:07 -0700

    Adjustet header-wrapper color to look more like old color

--- config.rb
+++ config.rb
@@ -4,9 +4,9 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "images"	
 javascripts_dir = "scripts"
-additional_import_paths = ["/Library/Ruby/Gems/1.8/gems/zen-grids-1.4/stylesheets"]
+additional_import_paths = ["/var/lib/gems/1.9.1/gems/zen-grids-1.4/stylesheets"]
 
 output_style = :expanded
 environment = :development
